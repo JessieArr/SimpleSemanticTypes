a4f25c47aef7f3e20ab3f9a09850a1644fe14bd1
Shawn Morrison <smorrison1986@gmail.com>
2016-02-06 20:46:34 -0600

    Adding Initial Implementation and Tests
    Ah, the halcyon days of 100% test coverage. How we will long for this
    moment someday. :)

---
+++ SimpleSemanticTypes.Tests/BaseSemanticTypeTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SimpleSemanticTypes.Tests
+{
+    [TestFixture]
+    public class BaseSemanticTypeTests
+    {
+        private string _TestString = "TestString";
+        private string _OtherTestString = "OtherTestString";
+        private int _TestInt = 5;
+        private int _OtherTestInt = 12;
+        private TestClass _TestClass = new TestClass();
+        private TestClass _OtherTestClass = new TestClass();
+
+        [Test]
+        public void Constructor_SucceedsForString()
+        {
+            var _SUT = new BaseSemanticType<string>(_TestString);
+        }
+
+        [Test]
+        public void Constructor_SucceedsForInt()
+        {
+            var _SUT = new BaseSemanticType<int>(_TestInt);
+        }
+
+        [Test]
+        public void Constructor_SucceedsForClass()
+        {
+            var _SUT = new BaseSemanticType<TestClass>(new TestClass());
+        }
+
+        [Test]
+        public void Constructor_ThrowsForNull()
+        {
+            Exception caughtException = null;
+            try
+            {
+                var _SUT = new BaseSemanticType<string>(null);
+            }
+            catch (Exception ex)
+            {
+                caughtException = ex;
+            }
+
+            Assert.NotNull(caughtException);
+            Assert.That(caughtException.Message == BaseSemanticType<string>.DefaultValidationMessage);
+        }
+
+        [Test]
+        public void EqualityComparison_SameValue_WorksForString()
+        {
+            var _SUT1 = new BaseSemanticType<string>(_TestString);
+            var _SUT2 = new BaseSemanticType<string>(_TestString);
+            Assert.That(_SUT1 == _SUT2);
+            Assert.False(_SUT1 != _SUT2);
+        }
+
+        [Test]
+        public void EqualityComparison_DifferentValue_WorksForString()
+        {
+            var _SUT1 = new BaseSemanticType<string>(_TestString);
+            var _SUT2 = new BaseSemanticType<string>(_OtherTestString);
+            Assert.False(_SUT1 == _SUT2);
+        }
+
+        [Test]
+        public void EqualityComparison_SameValue_WorksForInt()
+        {
+            var _SUT1 = new BaseSemanticType<int>(_TestInt);
+            var _SUT2 = new BaseSemanticType<int>(_TestInt);
+            Assert.That(_SUT1 == _SUT2);
+            Assert.False(_SUT1 != _SUT2);
+        }
+
+        [Test]
+        public void EqualityComparison_DifferentValue_WorksForInt()
+        {
+            var _SUT1 = new BaseSemanticType<int>(_TestInt);
+            var _SUT2 = new BaseSemanticType<int>(_OtherTestInt);
+            Assert.False(_SUT1 == _SUT2);
+        }
+
+        [Test]
+        public void EqualityComparison_SameValue_WorksForClass()
+        {
+            var _SUT1 = new BaseSemanticType<TestClass>(_TestClass);
+            var _SUT2 = new BaseSemanticType<TestClass>(_TestClass);
+            Assert.That(_SUT1 == _SUT2);
+            Assert.False(_SUT1 != _SUT2);
+        }
+
+        [Test]
+        public void EqualityComparison_DifferentValue_WorksForClass()
+        {
+            var _SUT1 = new BaseSemanticType<TestClass>(_TestClass);
+            var _SUT2 = new BaseSemanticType<TestClass>(_OtherTestClass);
+            Assert.False(_SUT1 == _SUT2);
+        }
+
+        [Test]
+        public void EqualityComparison_RightNull_WorksForClass()
+        {
+            var _SUT1 = new BaseSemanticType<TestClass>(_TestClass);
+            Assert.False(_SUT1 == null);
+            Assert.That(_SUT1 != null);
+        }
+
+        [Test]
+        public void EqualityComparison_LeftNull_WorksForClass()
+        {
+            var _SUT1 = new BaseSemanticType<TestClass>(_TestClass);
+            Assert.False(null == _SUT1);
+            Assert.That(null != _SUT1);
+        }
+
+        [Test]
+        public void GetHashCode_WorksForClass()
+        {
+            var _SUT1 = new BaseSemanticType<TestClass>(_TestClass);
+            var _SUT2 = new BaseSemanticType<TestClass>(_TestClass);
+            var _SUT3 = new BaseSemanticType<TestClass>(_OtherTestClass);
+
+            var hashcode1 = _SUT1.GetHashCode();
+            var hashcode2 = _SUT2.GetHashCode();
+            var hashcode3 = _SUT3.GetHashCode();
+
+            Assert.That(hashcode1 == hashcode2);
+            Assert.That(hashcode1 != hashcode3);
+        }
+
+        [Test]
+        public void Equals_WorksForClass()
+        {
+            var _SUT1 = new BaseSemanticType<TestClass>(_TestClass);
+            var _SUT2 = new BaseSemanticType<TestClass>(_TestClass);
+            var _SUT3 = new BaseSemanticType<TestClass>(_OtherTestClass);
+
+            Assert.That(_SUT1.Equals(_SUT2));
+            Assert.False(_SUT1.Equals(_SUT3));
+        }
+    }
+}
---
+++ SimpleSemanticTypes/BaseSemanticType.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSemanticTypes
+{
+    public class BaseSemanticType<T>
+    {
+        public readonly T Value;
+        public static string DefaultValidationMessage = "Wrapped " + typeof(T) + " value is not valid!";
+
+        public BaseSemanticType(T typeToWrap)
+        {
+            if (!ValueIsValid(typeToWrap))
+            {
+                var errorMessage = GetValidationFailureMessage(typeToWrap);
+                throw new NetTypeWrapperException(errorMessage);
+            }
+            Value = typeToWrap;
+        }
+
+        public static bool operator ==(BaseSemanticType<T> typeA, BaseSemanticType<T> typeB)
+        {
+            if (object.ReferenceEquals(typeA, null))
+            {
+                return object.ReferenceEquals(typeB, null);
+            }
+            else
+            {
+                if (object.ReferenceEquals(typeB, null))
+                {
+                    return false;
+                }
+            }
+
+            return typeA.Equals(typeB);
+        }
+
+        public static bool operator !=(BaseSemanticType<T> typeA, BaseSemanticType<T> typeB)
+        {
+            if (object.ReferenceEquals(typeA, null))
+            {
+                return !object.ReferenceEquals(typeB, null);
+            }
+            else
+            {
+                if (object.ReferenceEquals(typeB, null))
+                {
+                    return true;
+                }
+            }
+
+            return !typeA.Equals(typeB);
+        }
+
+        protected bool Equals(BaseSemanticType<T> other)
+        {
+            return EqualityComparer<T>.Default.Equals(Value, other.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((BaseSemanticType<T>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<T>.Default.GetHashCode(Value);
+        }
+
+        protected virtual bool ValueIsValid(T value)
+        {
+            if (object.ReferenceEquals(value, null))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        protected virtual string GetValidationFailureMessage(T value)
+        {
+            return DefaultValidationMessage;
+        }
+    }
+}
