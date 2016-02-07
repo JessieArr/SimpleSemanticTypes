829aa70828427871e631c411c90aee60fbffca34
Shawn Morrison <smorrison1986@gmail.com>
2016-02-07 12:49:16 -0600

    Implementing Other Arithmetic Operators
    Also improving test coverage. Somehow I forgot the happy path last time.
    Go figure.

--- SimpleSemanticTypes.Tests/ArithmeticSemanticTypeTests.cs
+++ SimpleSemanticTypes.Tests/ArithmeticSemanticTypeTests.cs
@@ -12,6 +12,20 @@ namespace SimpleSemanticTypes.Tests
     [TestFixture]
     public class ArithmeticSemanticTypeTests
     {
+        private int _TestInt = 5;
+        private int _OtherTestInt = 12;
+
+        [Test]
+        public void Addition_ArithmeticWrappedType_Succeeds()
+        {
+            var _SUT1 = new ArithmeticSemanticType<int>(_TestInt);
+            var _SUT2 = new ArithmeticSemanticType<int>(_OtherTestInt);
+
+            Assert.That(_SUT1 + _SUT2 == _TestInt + _OtherTestInt);
+            Assert.That(_SUT1 + _OtherTestInt == _TestInt + _OtherTestInt);
+            Assert.That(_TestInt + _SUT2 == _TestInt + _OtherTestInt);
+        }
+
         [Test]
         public void Addition_NonArithmeticWrappedType_Throws()
         {
@@ -54,6 +68,174 @@ namespace SimpleSemanticTypes.Tests
             Assert.Throws<NullReferenceException>(() =>
             {
                 var result = (TestClass)null + _SUT1;
+            });
+        }
+
+        [Test]
+        public void Subtraction_ArithmeticWrappedType_Succeeds()
+        {
+            var _SUT1 = new ArithmeticSemanticType<int>(_TestInt);
+            var _SUT2 = new ArithmeticSemanticType<int>(_OtherTestInt);
+
+            Assert.That(_SUT1 - _SUT2 == _TestInt - _OtherTestInt);
+            Assert.That(_SUT1 - _OtherTestInt == _TestInt - _OtherTestInt);
+            Assert.That(_TestInt - _SUT2 == _TestInt - _OtherTestInt);
+        }
+
+        [Test]
+        public void Subtraction_NonArithmeticWrappedType_Throws()
+        {
+            var testClass1 = new TestClass();
+            var _SUT1 = new ArithmeticSemanticType<TestClass>(testClass1);
+            var _SUT2 = new ArithmeticSemanticType<TestClass>(new TestClass());
+
+            Assert.Throws<SimpleSemanticTypeException>(() =>
+            {
+                var result = _SUT1 - _SUT2;
+            });
+            Assert.Throws<SimpleSemanticTypeException>(() =>
+            {
+                var result = _SUT1 - testClass1;
+            });
+            Assert.Throws<SimpleSemanticTypeException>(() =>
+            {
+                var result = testClass1 - _SUT1;
+            });
+        }
+
+        [Test]
+        public void Subtraction_ReferenceTypeNull_HandledCorrectly()
+        {
+            var _SUT1 = new ArithmeticSemanticType<TestClass>(new TestClass());
+
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = _SUT1 - (ArithmeticSemanticType<TestClass>)null;
+            });
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = (ArithmeticSemanticType<TestClass>)null - _SUT1;
+            });
+
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = _SUT1 - (TestClass)null;
+            });
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = (TestClass)null - _SUT1;
+            });
+        }
+
+        [Test]
+        public void Multiplication_ArithmeticWrappedType_Succeeds()
+        {
+            var _SUT1 = new ArithmeticSemanticType<int>(_TestInt);
+            var _SUT2 = new ArithmeticSemanticType<int>(_OtherTestInt);
+
+            Assert.That(_SUT1 * _SUT2 == _TestInt * _OtherTestInt);
+            Assert.That(_SUT1 * _OtherTestInt == _TestInt * _OtherTestInt);
+            Assert.That(_TestInt * _SUT2 == _TestInt * _OtherTestInt);
+        }
+
+        [Test]
+        public void Multiplication_NonArithmeticWrappedType_Throws()
+        {
+            var testClass1 = new TestClass();
+            var _SUT1 = new ArithmeticSemanticType<TestClass>(testClass1);
+            var _SUT2 = new ArithmeticSemanticType<TestClass>(new TestClass());
+
+            Assert.Throws<SimpleSemanticTypeException>(() =>
+            {
+                var result = _SUT1 * _SUT2;
+            });
+            Assert.Throws<SimpleSemanticTypeException>(() =>
+            {
+                var result = _SUT1 * testClass1;
+            });
+            Assert.Throws<SimpleSemanticTypeException>(() =>
+            {
+                var result = testClass1 * _SUT1;
+            });
+        }
+
+        [Test]
+        public void Multiplication_ReferenceTypeNull_HandledCorrectly()
+        {
+            var _SUT1 = new ArithmeticSemanticType<TestClass>(new TestClass());
+
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = _SUT1 * (ArithmeticSemanticType<TestClass>)null;
+            });
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = (ArithmeticSemanticType<TestClass>)null * _SUT1;
+            });
+
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = _SUT1 * (TestClass)null;
+            });
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = (TestClass)null * _SUT1;
+            });
+        }
+
+        [Test]
+        public void Division_ArithmeticWrappedType_Succeeds()
+        {
+            var _SUT1 = new ArithmeticSemanticType<int>(_TestInt);
+            var _SUT2 = new ArithmeticSemanticType<int>(_OtherTestInt);
+
+            Assert.That(_SUT1 / _SUT2 == _TestInt / _OtherTestInt);
+            Assert.That(_SUT1 / _OtherTestInt == _TestInt / _OtherTestInt);
+            Assert.That(_TestInt / _SUT2 == _TestInt / _OtherTestInt);
+        }
+
+        [Test]
+        public void Division_NonArithmeticWrappedType_Throws()
+        {
+            var testClass1 = new TestClass();
+            var _SUT1 = new ArithmeticSemanticType<TestClass>(testClass1);
+            var _SUT2 = new ArithmeticSemanticType<TestClass>(new TestClass());
+
+            Assert.Throws<SimpleSemanticTypeException>(() =>
+            {
+                var result = _SUT1 / _SUT2;
+            });
+            Assert.Throws<SimpleSemanticTypeException>(() =>
+            {
+                var result = _SUT1 / testClass1;
+            });
+            Assert.Throws<SimpleSemanticTypeException>(() =>
+            {
+                var result = testClass1 / _SUT1;
+            });
+        }
+
+        [Test]
+        public void Division_ReferenceTypeNull_HandledCorrectly()
+        {
+            var _SUT1 = new ArithmeticSemanticType<TestClass>(new TestClass());
+
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = _SUT1 / (ArithmeticSemanticType<TestClass>)null;
+            });
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = (ArithmeticSemanticType<TestClass>)null / _SUT1;
+            });
+
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = _SUT1 / (TestClass)null;
+            });
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var result = (TestClass)null / _SUT1;
             });
         }
     }
--- SimpleSemanticTypes/ArithmeticSemanticType.cs
+++ SimpleSemanticTypes/ArithmeticSemanticType.cs
@@ -13,7 +13,7 @@ namespace SimpleSemanticTypes
         {
         }
 
-        #region
+        #region + Operator Overloads
 
         internal static string NullAdditionArgumentErrorMessage = "Operand to operator '+' was null.";
 
@@ -30,7 +30,7 @@ namespace SimpleSemanticTypes
             }
             catch (Exception ex)
             {
-                var errorMessage = GetAdditionTypeErrorMessage(typeA.GetType(), typeB.GetType());
+                var errorMessage = GetAdditionTypeErrorMessage(typeA.Value.GetType(), typeB.Value.GetType());
                 throw new SimpleSemanticTypeException(errorMessage, ex);
             }
         }
@@ -48,7 +48,7 @@ namespace SimpleSemanticTypes
             }
             catch (Exception ex)
             {
-                var errorMessage = GetAdditionTypeErrorMessage(typeA.GetType(), typeB.GetType());
+                var errorMessage = GetAdditionTypeErrorMessage(typeA.Value.GetType(), typeB.GetType());
                 throw new SimpleSemanticTypeException(errorMessage, ex);
             }
         }
@@ -66,7 +66,7 @@ namespace SimpleSemanticTypes
             }
             catch (Exception ex)
             {
-                var errorMessage = GetAdditionTypeErrorMessage(typeA.GetType(), typeB.GetType());
+                var errorMessage = GetAdditionTypeErrorMessage(typeA.GetType(), typeB.Value.GetType());
                 throw new SimpleSemanticTypeException(errorMessage, ex);
             }
         }
@@ -74,6 +74,201 @@ namespace SimpleSemanticTypes
         private static string GetAdditionTypeErrorMessage(Type typeA, Type typeB)
         {
             return "Operator '+' not implemented for operands of type " + typeA.Name + " and " + typeB.Name + ".";
+        }
+
+        #endregion
+
+        #region  - Operator Overloads
+
+        internal static string NullSubtractionArgumentErrorMessage = "Operand to operator '-' was null.";
+
+        public static T operator -(ArithmeticSemanticType<T> typeA, ArithmeticSemanticType<T> typeB)
+        {
+            if (object.ReferenceEquals(typeA, null) || object.ReferenceEquals(typeB, null))
+            {
+                throw new NullReferenceException(NullAdditionArgumentErrorMessage);
+            }
+
+            try
+            {
+                return (dynamic)typeA.Value - (dynamic)typeB.Value;
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = GetSubtractionTypeErrorMessage(typeA.Value.GetType(), typeB.Value.GetType());
+                throw new SimpleSemanticTypeException(errorMessage, ex);
+            }
+        }
+
+        public static T operator -(ArithmeticSemanticType<T> typeA, T typeB)
+        {
+            if (object.ReferenceEquals(typeA, null) || object.ReferenceEquals(typeB, null))
+            {
+                throw new NullReferenceException(NullAdditionArgumentErrorMessage);
+            }
+
+            try
+            {
+                return (dynamic)typeA.Value - (dynamic)typeB;
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = GetSubtractionTypeErrorMessage(typeA.Value.GetType(), typeB.GetType());
+                throw new SimpleSemanticTypeException(errorMessage, ex);
+            }
+        }
+
+        public static T operator -(T typeA, ArithmeticSemanticType<T> typeB)
+        {
+            if (object.ReferenceEquals(typeA, null) || object.ReferenceEquals(typeB, null))
+            {
+                throw new NullReferenceException(NullAdditionArgumentErrorMessage);
+            }
+
+            try
+            {
+                return (dynamic)typeA - (dynamic)typeB.Value;
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = GetSubtractionTypeErrorMessage(typeA.GetType(), typeB.Value.GetType());
+                throw new SimpleSemanticTypeException(errorMessage, ex);
+            }
+        }
+
+        private static string GetSubtractionTypeErrorMessage(Type typeA, Type typeB)
+        {
+            return "Operator '-' not implemented for operands of type " + typeA.Name + " and " + typeB.Name + ".";
+        }
+
+        #endregion
+
+        #region  * Operator Overloads
+
+        internal static string NullMultiplicationArgumentErrorMessage = "Operand to operator '*' was null.";
+
+        public static T operator *(ArithmeticSemanticType<T> typeA, ArithmeticSemanticType<T> typeB)
+        {
+            if (object.ReferenceEquals(typeA, null) || object.ReferenceEquals(typeB, null))
+            {
+                throw new NullReferenceException(NullAdditionArgumentErrorMessage);
+            }
+
+            try
+            {
+                return (dynamic)typeA.Value * (dynamic)typeB.Value;
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = GetMultiplicationTypeErrorMessage(typeA.Value.GetType(), typeB.Value.GetType());
+                throw new SimpleSemanticTypeException(errorMessage, ex);
+            }
+        }
+
+        public static T operator *(ArithmeticSemanticType<T> typeA, T typeB)
+        {
+            if (object.ReferenceEquals(typeA, null) || object.ReferenceEquals(typeB, null))
+            {
+                throw new NullReferenceException(NullAdditionArgumentErrorMessage);
+            }
+
+            try
+            {
+                return (dynamic)typeA.Value * (dynamic)typeB;
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = GetMultiplicationTypeErrorMessage(typeA.Value.GetType(), typeB.GetType());
+                throw new SimpleSemanticTypeException(errorMessage, ex);
+            }
+        }
+
+        public static T operator *(T typeA, ArithmeticSemanticType<T> typeB)
+        {
+            if (object.ReferenceEquals(typeA, null) || object.ReferenceEquals(typeB, null))
+            {
+                throw new NullReferenceException(NullAdditionArgumentErrorMessage);
+            }
+
+            try
+            {
+                return (dynamic)typeA * (dynamic)typeB.Value;
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = GetMultiplicationTypeErrorMessage(typeA.GetType(), typeB.Value.GetType());
+                throw new SimpleSemanticTypeException(errorMessage, ex);
+            }
+        }
+
+        private static string GetMultiplicationTypeErrorMessage(Type typeA, Type typeB)
+        {
+            return "Operator '*' not implemented for operands of type " + typeA.Name + " and " + typeB.Name + ".";
+        }
+
+        #endregion
+
+        #region / Operator Overloads
+
+        internal static string NullDivisionArgumentErrorMessage = "Operand to operator '/' was null.";
+
+        public static T operator /(ArithmeticSemanticType<T> typeA, ArithmeticSemanticType<T> typeB)
+        {
+            if (object.ReferenceEquals(typeA, null) || object.ReferenceEquals(typeB, null))
+            {
+                throw new NullReferenceException(NullAdditionArgumentErrorMessage);
+            }
+
+            try
+            {
+                return (dynamic)typeA.Value / (dynamic)typeB.Value;
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = GetDivisionTypeErrorMessage(typeA.Value.GetType(), typeB.Value.GetType());
+                throw new SimpleSemanticTypeException(errorMessage, ex);
+            }
+        }
+
+        public static T operator /(ArithmeticSemanticType<T> typeA, T typeB)
+        {
+            if (object.ReferenceEquals(typeA, null) || object.ReferenceEquals(typeB, null))
+            {
+                throw new NullReferenceException(NullAdditionArgumentErrorMessage);
+            }
+
+            try
+            {
+                return (dynamic)typeA.Value / (dynamic)typeB;
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = GetDivisionTypeErrorMessage(typeA.Value.GetType(), typeB.GetType());
+                throw new SimpleSemanticTypeException(errorMessage, ex);
+            }
+        }
+
+        public static T operator /(T typeA, ArithmeticSemanticType<T> typeB)
+        {
+            if (object.ReferenceEquals(typeA, null) || object.ReferenceEquals(typeB, null))
+            {
+                throw new NullReferenceException(NullAdditionArgumentErrorMessage);
+            }
+
+            try
+            {
+                return (dynamic)typeA / (dynamic)typeB.Value;
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = GetDivisionTypeErrorMessage(typeA.GetType(), typeB.Value.GetType());
+                throw new SimpleSemanticTypeException(errorMessage, ex);
+            }
+        }
+
+        private static string GetDivisionTypeErrorMessage(Type typeA, Type typeB)
+        {
+            return "Operator '/' not implemented for operands of type " + typeA.Name + " and " + typeB.Name + ".";
         }
 
         #endregion
